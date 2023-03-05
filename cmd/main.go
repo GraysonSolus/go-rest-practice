5310676529ae8f9c67a69bc2ec9e45c62ddb9033
Galin Yordanov <gal.yordanov@gmail.com>
2023-03-05 22:33:34 +0000

    remove unnecessary method calls

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/GraysonSolus/go-rest-practice/controllers"
 )
 
 func main() {
 	fmt.Println("Starting Server")
-	http.ListenAndServe(":8080", nil)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/resize", controllers.ResizeImage)
+	mux.HandleFunc("/rotate", controllers.RotateImage)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
