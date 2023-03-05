5cc36b04f078a4a2ddac32d0bb89997ed2415281
Galin Yordanov <gal.yordanov@gmail.com>
2023-03-05 19:49:10 +0000

    add rotation handler

--- controllers/file-modification-controller.go
+++ controllers/file-modification-controller.go
@@ -3,6 +3,8 @@ package filemod
 import (
 	"fmt"
 	"image"
+	"image/color"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -89,6 +91,7 @@ func resizeImage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resized := imaging.Resize(rawImg, width, height, imaging.Lanczos)
+	imaging.Rotate()
 	// err = imaging.Save(resized, fmt.Sprintf("public/single/%v", "resizedImg.png"))
 	// if err != nil {
 	// 	log.Fatalf("failed to save image: %v", err)
@@ -104,4 +107,83 @@ func resizeImage(w http.ResponseWriter, r *http.Request) {
 
 	// return that we have successfully uploaded our file!
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func rotateImage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	r.ParseMultipartForm(10 << 20)
+
+	rawDegrees := r.FormValue("degrees")
+	degrees, err := strconv.ParseFloat(rawDegrees, 32)
+	if err != nil {
+		fmt.Println("Failed to parse degrees with value " + rawDegrees)
+
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	file, fileHeader, err := r.FormFile("image")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	defer file.Close()
+
+	// TODO: move this to a security package
+	buff := make([]byte, 512)
+	_, err = file.Read(buff)
+	if err != nil {
+		fmt.Println(err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	// checking the content type
+	// so we don't allow files other than images
+	filetype := http.DetectContentType(buff)
+	if filetype != "image/jpeg" && filetype != "image/png" && filetype != "image/jpg" {
+		fmt.Println("The provided file format is not allowed. Please upload a JPEG,JPG or PNG image")
+
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	// TODO: Move this into an image manipulation service
+	rawImg, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("Failed to decode image from file")
+
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	rotated := imaging.Rotate(rawImg, degrees, color.Transparent)
+	err = imaging.Save(rotated, fmt.Sprintf("public/single/%v", "rotatedImg.jpg"))
+	if err != nil {
+		log.Fatalf("failed to save image: %v", err)
+	}
+
+	fmt.Printf("Uploaded File: %+v\n", fileHeader.Filename)
+	fmt.Printf("File Size: %+v\n", fileHeader.Size)
+	fmt.Printf("MIME Header: %+v\n", fileHeader.Header)
+
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	imaging.Encode(w, rotated, imaging.PNG)
 }
